606806efdedb334e85b021035fe18de3f7bf1c58
Elizaveta <mefmag01@gmail.com>
2025-11-07 10:18:51 +0000

    Добавлено исключение, когда степень в fast_pow отрицательная, учтена нечетная степень

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,12 @@
 def fastPow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    if power<0: 
+        raise ValueError("Введена отрицательная степень")
+    if power==0:
+        return 1
+    result = 1
+    while power > 0:
+        if (power%2)!=0:
+            result *= number
+        number *= number
         power = power // 2
     return result
