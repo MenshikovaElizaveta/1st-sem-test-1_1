6ae37780784d1cd2ec3766f2710ea93b3eb7c5a2
Elizaveta <mefmag01@gmail.com>
2025-11-07 10:41:48 +0000

    Добавлены тесты fast_pow, покрывающие ошибки.

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,3 +1,4 @@
+import pytest
 from src.fast_pow import fastPow
 
 
@@ -7,3 +8,16 @@ def test_two_power_two():
 
 def test_negative():
     assert fastPow(-1, 4) == 1
+    
+
+def test_power_zero():
+    assert fastPow(58, 0) == 1
+    
+
+def test_odd_degree():
+    assert fastPow(2, 5) == 32
+    
+    
+def test_negative_power():
+    with pytest.raises(ValueError, match="Введена отрицательная степень"):
+        fastPow(2, -1)
